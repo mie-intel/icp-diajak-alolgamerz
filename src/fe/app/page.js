6c5f60392804e1fdbc820dad643419b10cf6f0fa
mie-intel <paryapradhanika@gmail.com>
2025-01-27 23:22:00 +0700

    feat(font): menambahkan font

--- src/fe/app/page.js
+++ src/fe/app/page.js
@@ -4,7 +4,8 @@ import WebCamera from "@/components/UI/WebCamera";
 export default function Home() {
   return (
     <DefaultLayout>
-      <WebCamera className="relative h-[1000px] w-[1000px] bg-[black]" />
+      {/* <WebCamera className="relative h-[1000px] w-[1000px] bg-[black]" /> */}
+      <div className="font-dmSans h-full w-full text-[50px] font-[700] text-black">Register</div>
     </DefaultLayout>
   );
 }
