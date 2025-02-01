6864f3d34ab4121c61a22adbf91fb911378744cb
Troll321 <atila.ghulwani@gmail.com>
2025-02-01 10:21:42 +0700

    Expose ipfs API

---
+++ src/be/modules/ipfs.js
@@ -0,0 +1,9 @@
+/**
+ * Upload data to ipfs and returns file URL
+ * @todo
+ * @param {String} data -- base64 encoded data
+ * @returns {String} -- fileURL
+ */
+export async function upload(data) {
+    return "http://localhost";
+}
